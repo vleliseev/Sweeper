bd1a96b16dcf9bdac4c1de2c02fd270aa4fc4e09
Vladislav <shmel3@openmailbox.org>
2018-08-20 21:08:23 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,62 @@
+/**
+ * Object of item in browser context menu
+ * 
+ * @type {Object}
+ */
+const mainMenuItem = {
+    id: 'sweeper-addon',
+    title: 'Sweeper',
+    contexts: ['all']
+}
+
+/**
+ * Object of dialog item that appears
+ * on 'Sweeper' context menu item selection;
+ * Releases washing of element(s)
+ * 
+ * @type {Object}
+ */
+const cleanDialogItem = {
+    parentId: 'sweeper-addon',
+    id: 'sweeper-clean',
+    title: 'Wash',
+    icons: { "16": "res/wash.svg" },
+    contexts: ['all']
+}
+
+/**
+ * Object of dialog item that appears
+ * on 'Sweeper' context menu item selection;
+ * Releases removing of selected element
+ * 
+ * @type {Object}
+ */
+const eraseDialogItem = {
+    parentId: 'sweeper-addon',
+    id: 'sweeper-erase',
+    title: 'Erase',
+    icons: { "16": "res/erase.svg" },
+    contexts: ['all']
+}
+
+
+/**
+ * Listen for context menu items click
+ * and send message to content script (sweeper.js) 
+ * with selected action
+ * 
+ * @param {OnClickData} clicked 
+ * @param {Tab} tab 
+ */
+function contextMenuListener(clicked, tab) {
+    if (clicked.menuItemId === "sweeper-clean") {
+        browser.tabs.sendMessage(tab.id, { action: "wash" });
+    } else if (clicked.menuItemId === "sweeper-erase") {
+        browser.tabs.sendMessage(tab.id, { action: "erase" });
+    }
+}
+
+browser.contextMenus.create(mainMenuItem);
+browser.contextMenus.create(cleanDialogItem);
+browser.contextMenus.create(eraseDialogItem);
+browser.contextMenus.onClicked.addListener(contextMenuListener);
---
+++ sweeper.js
@@ -0,0 +1,74 @@
+
+/**
+ * listen for context menu event
+ * and set nodeToClean to currently selected node
+ * 
+ * @param {Event} e 
+ */
+function rightClickListener(e) {
+    nodeToClean = e.target;
+}
+
+
+/**
+ * recursively find element with 
+ * filter property and set it 
+ * to none to make element(s) visible
+ * 
+ * @param {Element} element 
+ */
+function wash(element) {
+    // computed style of current element 
+    let style = window.getComputedStyle(element);
+    
+    if (style['filter'] && style['filter'] !== 'none') {
+        
+        // set filter property value to 'none' if it
+        // is not empty or 'none' already
+        element.style.filter = 'none';
+    } else {
+
+        // check parent node for filter property
+        wash(element.parentElement);
+    }
+}
+
+
+/**
+ * remove selected element from page
+ * 
+ * @param {Element} element 
+ */
+function erase(element) {
+    element.parentElement.removeChild(element);
+}
+
+
+/**
+ * listen for message from background script
+ * and wash/erase selected element
+ * 
+ * @param {Object} message 
+ */
+function onMessage(message) {
+
+    if (message.action === "wash") {
+        wash(nodeToClean);
+
+    } else if (message.action === "erase") {
+        erase(nodeToClean);
+
+    }
+}
+
+/**
+ * last element that was selected
+ * with context menu event (right click)
+ * 
+ * @type {Element}
+ */
+var nodeToClean;
+
+
+document.addEventListener('contextmenu', rightClickListener);
+browser.runtime.onMessage.addListener(onMessage);
